b66239af326dbd859b3b2c2f6a5850302b1e72a1
SeeQ <sxdz1290@naver.com>
2023-12-01 15:23:34 +0900

    검색옵션 작성자 주석처리

--- frontend/react-spring-blog-frontend-main/src/Components/bbs/BbsList.js
+++ frontend/react-spring-blog-frontend-main/src/Components/bbs/BbsList.js
@@ -96,7 +96,7 @@ function BbsList() {
                             <option>검색 옵션</option>
                             <option value="title">제목</option>
                             <option value="content">내용</option>
-                            <option value="writer">작성자</option>
+                            {/*<option value="writer">작성자</option>*/}
                         </select>
                     </td>
                     <td>
